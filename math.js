ac0729e0198ab8fe51a88516de4045823f701f85
aysha3f <tahiaysha220@gmail.com>
2025-04-03 23:26:36 +0600

    Math, abs, round, ceil, floor, and random number

---
+++ math.js
@@ -0,0 +1,27 @@
+// const Math = {
+//     min: function min (num1, num2){
+//         return num2;
+//     }
+// }
+
+//  console.log(Math.max);
+
+
+const min = Math.min(45, 1, 65, 99, 126, 5, -99)
+// console.log(min);
+const max = Math.max(45, 1, 65, 99, 126, 5, -99)
+// console.log(max)
+
+// console.log(Math.PI)
+// console.log(Math.abs(23-4));
+//  console.log(Math.abs(-999));
+//  console.log(Math.round(7.888));
+//  console.log(Math.round(7.248));
+
+//  console.log(Math.floor(2.95555555555));
+//  console.log(Math.ceil(2.00001));
+
+// console.log(Math.random())
+console.log(Math.random()*10)
+const rand = Math.round(Math.random ()*10)
+console.log(rand)
